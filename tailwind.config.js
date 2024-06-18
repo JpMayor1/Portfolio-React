3c7db4a644d9d5dde30091900a1b79e41aab218e
James Phillip Mayor <117265110+JpMayor1@users.noreply.github.com>
2024-06-18 15:40:11 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        colors: {
+            black: "#151515",
+            white: "#ffffff",
+            primary: "#40A2D8",
+            secondary: "#58287F",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
